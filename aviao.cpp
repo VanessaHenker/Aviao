5d81d2c4ec1cddb1f832c2f96e10b8b3c453d302
Vanessa <vs6615302@gmail.com>
2023-07-22 17:00:31 -0300

    Adicionando destinos na função destino

--- aviao.cpp
+++ aviao.cpp
@@ -12,5 +12,13 @@ int main() {
 }
 
 void destinos(){
+setlocale(LC_ALL, "Portuguese_Brazil");
+lugares[0] = "Rio de Janeiro", "rio de janeiro";
+lugares[1] = "SÃO Paulo", "são paulo", "Sao Paulo", "sao paulo";
+lugares[2] = "Belém", "Belem", "belem", "belém";
+lugares[3] = "Brasília", "Brasilia", "brasilia", "brasília";
+lugares[4] = "Cuiabá", "Cuiaba", "cuiabá", "cuiaba",
+lugares[5] = "Curitiba", "curitida";
+
 
 }
