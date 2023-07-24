1589bf91b8a55ad154d2ac62df5bb4ad8c447978
Vanessa <vs6615302@gmail.com>
2023-07-24 16:05:01 -0300

    Excluindo a função destino

--- aviao.cpp
+++ aviao.cpp
@@ -2,7 +2,6 @@
 #include <string>
 using namespace std;
 
-void destinos();
 void mostrar_nome();
 void escolher_lugar();
 string nomes[30], lugares[30], origem, destino;
@@ -14,52 +13,17 @@ int main() {
  cout << "\n    Mais de 30 lugares para você escolher!" << endl;
  cout << endl;
  mostrar_nome();
-
+ escolher_lugar();
  return 0;
 }
 
-void destinos(){
-setlocale(LC_ALL, "Portuguese_Brazil");
-lugares[0] = "Rio de Janeiro", "rio de janeiro";
-lugares[1] = "SÃO Paulo", "são paulo", "Sao Paulo", "sao paulo";
-lugares[2] = "Belém", "Belem", "belem", "belém";
-lugares[3] = "Brasília", "Brasilia", "brasilia", "brasília";
-lugares[4] = "Cuiabá", "Cuiaba", "cuiabá", "cuiaba",
-lugares[5] = "Curitiba", "curitida";
-lugares[6] =  "Miami", "miami";
-lugares[7] = "Fort Lauderdale", "fort lauderdale";
-lugares[8] = "Florianópolis", "Florianopolis","floarianopolis";
-lugares[9] = "Foartaleza", "fortaleza";
-lugares[10] = "Goiânia", "Goiania", "goiania";
-lugares[11] = "Lisboa", "lisboa";
-lugares[12] = "Manaus", "manaus";
-lugares[13] = "Orlando", "orlando";
-lugares[14] = "Carrasco" "carrasco";
-lugares[15] = "Paris", "paris";
-lugares[16] = "Porto Alegre", "Porto alegre", "porto alegre";
-lugares[17] = "Porto Velho", "Porto velho", "porto velho";
-lugares[18] = "Recife", "recife";
-lugares[19] = "Santos Dumont"; "Santos dumont", "santos dumont";
-lugares[20] = "São Luíz"; "são luíz", "sao luiz";
-lugares[21] = "Vitória", "vitória", "vitoria";
-lugares[22] = "Málaga", "malaga", "málaga";
-lugares[23] = "Aracaju", "aracaju";
-lugares[24] = "Barcelona", "barcelona";
-lugares[25] = "João Pessoa", "Joao Pessoa", "joao pessoa", "joão pessoa";
-lugares[26] = "Los Angeles", "los angeles", "Los angeles";
-lugares[27] = "New York", "new york";
-lugares[28] = "Alemanha", "alemanha"; 
-lugares[29] = "Argentina","argentina";
-lugares[30] = "França", "frança", "franca";
-lugares[31] = "Dubai", "dubai";
-}
-
 void mostrar_nome(){
-nomes[0] = "Rio de Janeiro";
-nomes[1] = "São Paulo";
-nomes[2] = "Belém";
-nomes[3] = "Brasília";
-nomes[4] = "Cuiabá";
+setlocale(LC_ALL, "Portuguese_Brazil");
+nomes[0] = "[1]Rio de Janeiro";
+nomes[1] = "[2]São Paulo";
+nomes[2] = "[3]Belém";
+nomes[3] = "[4]Brasília";
+nomes[4] = "[5]Cuiabá";
 nomes[5] = "Curitiba";
 nomes[6] = "Miami";
 nomes[7] = "Fort Lauderdale";
@@ -123,15 +87,22 @@ cout << nomes[31] << endl;
 }
 
 void escolher_lugar(){
+setlocale(LC_ALL, "Portuguese_Brazil");
+
 int i;
+i = 0;
 cout << endl;
 cout << endl;
-cout << "Origem? ";
-cin >> origem;
+cout << "ORIGEM? ";
+getline(cin,origem);
+cout << origem;
 for (i = 0; i < 31; i++){
  if(origem == lugares[i]){
-  cout << "Destino? ";
+  cout << "\nDestino? ";
   cin >> destino;
+ }
+ else{
+  cout << "\nErro, tente novamente!" << endl;
  }
 }
 
