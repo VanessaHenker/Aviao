469c24ab47e6b371882aee0ea56c5e2e75e5279e
Vanessa <vs6615302@gmail.com>
2023-07-24 14:46:48 -0300

    Adicionando a função mostrar_nome

--- aviao.cpp
+++ aviao.cpp
@@ -2,7 +2,10 @@
 #include <string>
 using namespace std;
 
-string lugares[30], origem;
+void destinos();
+void mostrar_nome();
+
+string nomes[30], lugares[30], origem;
 
 int main() {
   setlocale(LC_ALL, "Portuguese_Brazil");
@@ -33,7 +36,7 @@ lugares[14] = "Carrasco" "carrasco";
 lugares[15] = "Paris", "paris";
 lugares[16] = "Porto Alegre", "Porto alegre", "porto alegre";
 lugares[17] = "Porto Velho", "Porto velho", "porto velho";
-lugares[18] = "Rececife", "recife";
+lugares[18] = "Recife", "recife";
 lugares[19] = "Santos Dumont"; "Santos dumont", "santos dumont";
 lugares[20] = "São Luíz"; "são luíz", "sao luiz";
 lugares[21] = "Vitória", "vitória", "vitoria";
@@ -44,6 +47,40 @@ lugares[25] = "João Pessoa", "Joao Pessoa", "joao pessoa", "joão pessoa";
 lugares[26] = "Los Angeles", "los angeles", "Los angeles";
 lugares[27] = "New York", "new york";
 lugares[28] = "Alemanha", "alemanha"; 
-lugares[29] = "Argentina" "argentina";
+lugares[29] = "Argentina","argentina";
 lugares[30] = "França", "frança", "franca";
+}
+
+void mostrar_nome(){
+nomes[0] = "Rio de Janeiro";
+nomes[1] = "São Paulo";
+nomes[2] = "Belém";
+nomes[3] = "Brasília";
+nomes[4] = "Cuiabá";
+nomes[5] = "Curitiba";
+nomes[6] = "Miami";
+nomes[7] = "Fort Lauderdale";
+nomes[8] = "Florianópolis";
+nomes[9] = "Foartaleza";
+nomes[10] = "Goiânia";
+nomes[11] = "Lisboa";
+nomes[12] = "Manaus";
+nomes[13] = "Orlando";
+nomes[14] = "Carrasco";
+nomes[15] = "Paris";
+nomes[16] = "Porto Alegre";
+nomes[17] = "Porto Velho";
+nomes[18] = "Recife";
+nomes[19] = "Santos Dumont";
+nomes[20] = "São Luíz";
+nomes[21] = "Vitória";
+nomes[22] = "Málaga";
+nomes[23] = "Aracaju";
+nomes[24] = "Barcelona";
+nomes[25] = "João Pessoa";
+nomes[26] = "Los Angeles";
+nomes[27] = "New York";
+nomes[28] = "Alemanha";
+nomes[29] = "Argentina";
+nomes[30] = "França";
 }
