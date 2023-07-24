13072d3c05853bee9713ce7e4fc642e972d7798e
Vanessa <vs6615302@gmail.com>
2023-07-24 16:53:08 -0300

    Adicionando a função lugar

--- aviao.cpp
+++ aviao.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 void mostrar_nome();
-void escolher_lugar();
-string nomes[30], lugares[30];
+void lugar();
+void escolha_lugar();
+string nomes[31], lugares[31];
 int origem, destino;
 
 int main() {
@@ -21,16 +22,16 @@ int main() {
 void mostrar_nome(){
 setlocale(LC_ALL, "Portuguese_Brazil");
 int i = 0;
-nomes[0] = " [1]->  Rio de Janeiro";
-nomes[1] = " [2]->  São Paulo";
-nomes[2] = " [3]->  Belém";
-nomes[3] = " [4]->  Brasília";
-nomes[4] = " [5]->  Cuiabá";
-nomes[5] = " [6]->  Curitiba";
-nomes[6] = " [7]->  Miami";
-nomes[7] = " [8]->  Fort Lauderdale";
-nomes[8] = " [9]->  Florianópolis";
-nomes[9] = " [10]-> Foartaleza";
+nomes[0] =  "[1]->  Rio de Janeiro";
+nomes[1] =  "[2]->  São Paulo";
+nomes[2] =  "[3]->  Belém";
+nomes[3] =  "[4]->  Brasília";
+nomes[4] =  "[5]->  Cuiabá";
+nomes[5] =  "[6]->  Curitiba";
+nomes[6] =  "[7]->  Miami";
+nomes[7] =  "[8]->  Fort Lauderdale";
+nomes[8] =  "[9]->  Florianópolis";
+nomes[9] =  "[10]-> Foartaleza";
 nomes[10] = "[11]-> Goiânia";
 nomes[11] = "[12]-> Lisboa";
 nomes[12] = "[13]-> Manaus";
@@ -91,6 +92,105 @@ lugares[30] = "França";
 lugares[31] = "Dubai";
 }
 
+void lugar(){
+setlocale(LC_ALL, "Portuguese_Brazil");
+if(origem == 1){
+cout << lugares[0] << endl;
+}
+if(origem == 2){
+cout << lugares[1] << endl;
+}
+if(origem == 3){
+cout << lugares[2] << endl;
+}
+if(origem == 4){
+cout << lugares[3] << endl;
+}
+if(origem == 5){
+cout << lugares[4] << endl;
+}
+if(origem == 6){
+cout << lugares[5] << endl;
+}
+if(origem == 7){
+cout << lugares[6] << endl;
+}
+if(origem == 8){
+cout << lugares[7] << endl;
+}
+if(origem == 9){
+cout << lugares[8] << endl;
+}
+if(origem == 10){
+cout << lugares[9] << endl;
+}
+if(origem == 11){
+cout << lugares[10] << endl;
+}
+if(origem == 12){
+cout << lugares[11] << endl;
+}
+if(origem == 13){
+cout << lugares[12] << endl;
+}
+if(origem == 14){
+cout << lugares[13] << endl;
+}
+if(origem == 15){
+cout << lugares[14] << endl;
+}
+if(origem == 16){
+cout << lugares[15] << endl;
+}
+if(origem == 17){
+cout << lugares[16] << endl;
+}
+if(origem == 18){
+cout << lugares[17] << endl;
+}
+if(origem == 19){
+cout << lugares[18] << endl;
+}
+if(origem == 20){
+cout << lugares[19] << endl;
+}
+if(origem == 21){
+cout << lugares[20] << endl;
+}
+if(origem == 22){
+cout << lugares[21] << endl;
+}
+if(origem == 23){
+cout << lugares[22] << endl;
+}
+if(origem == 24){
+cout << lugares[23] << endl;
+}
+if(origem == 25){
+cout << lugares[24] << endl;
+}
+if(origem == 26){
+cout << lugares[25] << endl;
+}
+if(origem == 27){
+cout << lugares[26] << endl;
+}
+if(origem == 28){
+cout << lugares[27] << endl;
+}
+if(origem == 29){
+cout << lugares[28] << endl;
+}
+if(origem == 30){
+cout << lugares[29] << endl;
+}
+if(origem == 31){
+cout << lugares[30] << endl;
+}
+if(origem == 32){
+cout << lugares[31] << endl;
+}
+}
 void escolher_lugar(){
 setlocale(LC_ALL, "Portuguese_Brazil");
 
@@ -133,6 +233,7 @@ case 29:
 case 30:
 case 31:
 case 32:
+cout << lugares[31] << endl;
 cout << "\nDESTINO? ";
 cin >> destino;
 break;
