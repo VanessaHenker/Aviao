03fcbc5b44cd303c14ff05a98310d5c085708be9
Vanessa <vs6615302@gmail.com>
2023-07-24 18:48:57 -0300

    Alteração na função escolher_lugar

--- aviao.cpp
+++ aviao.cpp
@@ -96,53 +96,53 @@ setlocale(LC_ALL, "Portuguese_Brazil");
 
 int i;
 i = 0;
-cout << endl;
-cout << endl;
-cout << "\nORIGEM? ";
-cin >> origem;
-switch (origem){
-case 1:
-case 2:
-case 3:
-case 4:
-case 5:
-case 6:
-case 7:
-case 8:
-case 9:
-case 10:
-case 11:
-case 12:
-case 13:
-case 14:
-case 15:
-case 16:
-case 17:
-case 18:
-case 19:
-case 20:
-case 21:
-case 22:
-case 23:
-case 24:
-case 25:
-case 26:
-case 27:
-case 28:
-case 29:
-case 30:
-case 31:
-case 32:
-lugar();
-cout << endl;
-cout << "\nDESTINO? ";
-cin >> destino;
-break;
-default:
-  cout << "\nErro, tente novamente!" << endl;
-  break;
-}
+origem = 32;
 
+while(origem < 1 || origem > 31){
+ cout << "\nORIGEM? ";
+ cin >> origem;
+ switch (origem){
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+  case 8:
+  case 9:
+  case 10:
+  case 11:
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 22:
+  case 23:
+  case 24:
+  case 25:
+  case 26:
+  case 27:
+  case 28:
+  case 29:
+  case 30:
+  case 31:
+  case 32:
+  lugar();
+  cout << "\nDESTINO? ";
+  cin >> destino;
+  break;
+  default:
+    cout << "\nErro, tente novamente!" << endl;
+    break;
+    }
+  }
 }
 void lugar(){
 setlocale(LC_ALL, "Portuguese_Brazil");
