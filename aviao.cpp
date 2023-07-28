f51e526131e75991f7670e90edf7296bbc2bbd11
Vanessa <vs6615302@gmail.com>
2023-07-27 21:10:26 -0300

    Alterando alguns detalhes

--- aviao.cpp
+++ aviao.cpp
@@ -13,17 +13,17 @@ char destino_dnv;
 int origem, destino, escolha_mes;
 
 void printCalendar(int year, int month){
-  // Criar uma estrutura tm com a data do primeiro dia do mês
+  // Criar uma estrutura tm com a data do primeiro dia do mÃªs
   std::tm timeinfo = {};
-  timeinfo.tm_year = year - 1900; // tm_year é o número de anos desde 1900
-  timeinfo.tm_mon = month - 1;    // tm_mon é base 0 (janeiro é representado por 0)
+  timeinfo.tm_year = year - 1900; // tm_year Ã© o nÃºmero de anos desde 1900
+  timeinfo.tm_mon = month - 1;    // tm_mon Ã© base 0 (janeiro Ã© representado por 0)
   timeinfo.tm_mday = 1;
   std::mktime(&timeinfo);
 
-  // Obter o número de dias no mês atual
+  // Obter o nÃºmero de dias no mÃªs atual
   int daysInMonth;
   if (month == 2){
-    // Verificar se é um ano bissexto (fevereiro tem 29 dias em anos bissextos)
+    // Verificar se Ã© um ano bissexto (fevereiro tem 29 dias em anos bissextos)
     daysInMonth = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
   }
   else if (month == 4 || month == 6 || month == 9 || month == 11){
@@ -33,15 +33,15 @@ void printCalendar(int year, int month){
     daysInMonth = 31;
   }
 
-  // Imprimir o cabeçalho do calendário
+  // Imprimir o cabeÃ§alho do calendÃ¡rio
   std::cout << "  " << std::setw(11) << std::left << "DOM SEG TER QUA QUI SEX SAB" << std::endl;
 
-  // Imprimir os espaços iniciais para alinhar o primeiro dia do mês
+  // Imprimir os espaÃ§os iniciais para alinhar o primeiro dia do mÃªs
   for (int i = 0; i < timeinfo.tm_wday; ++i){
     std::cout << "    ";
   }
 
-  // Imprimir os dias do mês
+  // Imprimir os dias do mÃªs
   for (int day = 1; day <= daysInMonth; ++day){
     std::cout << std::setw(4) << std::right << day;
 
@@ -56,7 +56,7 @@ int main(){
   setlocale(LC_ALL, "Portuguese_Brazil");
 
   cout << "\n    PRA ONDE VAMOS? ";
-  cout << "\n    Mais de 30 lugares para você escolher!" << endl;
+  cout << "\n    Mais de 30 lugares para vocÃª escolher!" << endl;
   cout << endl;
   mostrar_nome();
   escolher_lugar();
@@ -67,16 +67,16 @@ void mostrar_nome(){
   setlocale(LC_ALL, "Portuguese_Brazil");
   int i = 0;
   lugares[0] = "Rio de Janeiro";
-  lugares[1] = "São Paulo";
-  lugares[2] = "Belém";
-  lugares[3] = "Brasília";
-  lugares[4] = "Cuiabá";
+  lugares[1] = "SÃ£o Paulo";
+  lugares[2] = "BelÃ©m";
+  lugares[3] = "BrasÃ­lia";
+  lugares[4] = "CuiabÃ¡";
   lugares[5] = "Curitiba";
   lugares[6] = "Miami";
   lugares[7] = "Fort Lauderdale";
-  lugares[8] = "Florianópolis";
+  lugares[8] = "FlorianÃ³polis";
   lugares[9] = "Foartaleza";
-  lugares[10] = "Goiânia";
+  lugares[10] = "GoiÃ¢nia";
   lugares[11] = "Lisboa";
   lugares[12] = "Manaus";
   lugares[13] = "Orlando";
@@ -86,17 +86,17 @@ void mostrar_nome(){
   lugares[17] = "Porto Velho";
   lugares[18] = "Recife";
   lugares[19] = "Santos Dumont";
-  lugares[20] = "São Luíz";
-  lugares[21] = "Vitória";
-  lugares[22] = "Málaga";
+  lugares[20] = "SÃ£o LuÃ­z";
+  lugares[21] = "VitÃ³ria";
+  lugares[22] = "MÃ¡laga";
   lugares[23] = "Aracaju";
   lugares[24] = "Barcelona";
-  lugares[25] = "João Pessoa";
+  lugares[25] = "JoÃ£o Pessoa";
   lugares[26] = "Los Angeles";
   lugares[27] = "New York";
   lugares[28] = "Alemanha";
   lugares[29] = "Argentina";
-  lugares[30] = "França";
+  lugares[30] = "FranÃ§a";
   lugares[31] = "Dubai";
   for (i < 0; i < 31; i++)
   {
@@ -150,9 +150,9 @@ void escolher_lugar(){
       cin >> destino;
       if (destino == origem){
         while (destino == origem && destino_dnv == 's' || destino_dnv == 'S'){
-          cout << "\nErro, você já selecionou " << lugares[origem] << " como origem!" << endl;
+          cout << "\nErro, vocÃª jÃ¡ selecionou " << lugares[origem] << " como origem!" << endl;
           cout << "\nSe deseja escolher outro destino ";
-          cout << "\nDigite 'S' para SIM ou 'N' para NÂO: ";
+          cout << "\nDigite 'S' para SIM ou 'N' para NÃ‚O: ";
           cin >> destino_dnv;
           if (destino_dnv == 's' || destino_dnv == 'S'){
             cout << "\nDESTINO? ";
@@ -180,7 +180,7 @@ void calendario(){
   name_month[0] = "Janeiro";
   name_month[1] = "Janeiro";
   name_month[2] = "Fevereiro";
-  name_month[3] = "Março";
+  name_month[3] = "MarÃ§o";
   name_month[4] = "Abril";
   name_month[5] = "Maio";
   name_month[6] = "Junho";
@@ -190,24 +190,25 @@ void calendario(){
   name_month[10] = "Outubro";
   name_month[11] = "Novembro";
   name_month[12] = "Dezembro";
-  // Obter o horário atual do sistema
+  // Obter o horÃ¡rio atual do sistema
   std::time_t currentTime = std::time(nullptr);
 
   // Converter para uma estrutura tm (hora local)
   std::tm *localTime = std::localtime(&currentTime);
 
-  // Extrair o mês e o ano atual
-  int month = localTime->tm_mon + 1;    // tm_mon é base 0 (janeiro é representado por 0)
-  int year = localTime->tm_year + 1900; // tm_year é o número de anos desde 1900
+  // Extrair o mÃªs e o ano atual
+  int month = localTime->tm_mon + 1;    // tm_mon Ã© base 0 (janeiro Ã© representado por 0)
+  int year = localTime->tm_year + 1900; // tm_year Ã© o nÃºmero de anos desde 1900
 
   char option;
   do{
-    // Exibir o calendário do mês atual
-    std::cout << "Calendário de " << month << "/" << year << std::endl;
+    // Exibir o calendÃ¡rio do mÃªs atual
+    cout << endl;
+    std::cout << "        " <<name_month[month] << "       " << month << "/" << year << std::endl;
     printCalendar(year, month);
 
-    // Pedir ao usuário para navegar para o mês seguinte ou anterior
-    std::cout << "N - Próximo mês, P - Mês anterior, Q - Sair" << std::endl;
+    // Pedir ao usuÃ¡rio para navegar para o mÃªs seguinte ou anterior
+    std::cout << "N - PrÃ³ximo mÃªs, P - MÃªs anterior, Q - Sair" << std::endl;
     std::cin >> option;
 
     if (option == 'N' || option == 'n'){
