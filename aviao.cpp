ca7728dccd26965ca156ce22bf3e476c5f4fbdaf
Vanessa <vs6615302@gmail.com>
2023-07-31 19:29:05 -0300

    Adicionando função opcao_selec

--- aviao.cpp
+++ aviao.cpp
@@ -11,6 +11,7 @@ void escolher_lugar();
 void escolher_passagem();
 void escolha_dia();
 void horario();
+void opcao_selec();
 // variáveis pra escolher o lugar
 int origem, destino, escolha_mes, escolha_lugar, opcao_lugar;
 string lugares[31], name_month[13];
@@ -62,12 +63,13 @@ void printCalendar(int year, int month){
 }
 int main(){
   setlocale(LC_ALL, "Portuguese_Brazil");
-  horario(); 
+  horario();
   cout << "\nPRA ONDE VAMOS? ";
   cout << "\nMais de 30 lugares para você escolher!" << endl;
   cout << endl;
   mostrar_nome();
   escolher_lugar();
+  escolher_passagem();
   return 0;
 }
 
@@ -249,46 +251,44 @@ void calendario(){
   option = 3;
   cout << endl;
   while (option != 2){
-    if(year < 2025 && month >= 7 || year == 2024 && month >= 1){ 
-    // Exibir o calendário do mês atual
-    cout << "\n------------------------------";
-    cout << "\n       DATA DAS VIAGENS";
-    cout << "\n------------------------------";
-    std::cout << "\n        " << name_month[month] << "       " << month << "/" << year << std::endl;
-    printCalendar(year, month);
+    if (year < 2025 && month >= 7 || year == 2024 && month >= 1){
+      // Exibir o calendário do mês atual
+      cout << "\n------------------------------";
+      cout << "\n       DATA DAS VIAGENS";
+      cout << "\n------------------------------";
+      std::cout << "\n        " << name_month[month] << "       " << month << "/" << year << std::endl;
+      printCalendar(year, month);
 
-    // Pedir ao usuário para navegar para o mês seguinte ou anterior
-    std::cout << " 0- Anterior, 1- Próximo, 2- Sair" << std::endl;
-    std::cin >> option;
+      // Pedir ao usuário para navegar para o mês seguinte ou anterior
+      std::cout << " 0- Anterior, 1- Próximo, 2- Sair" << std::endl;
+      std::cin >> option;
 
-  if (option == 1){
-    if(month == 12 && option == 1 && year == 2024){
-     month = 12;
-     }
-     else{ 
-      month++;
+      if (option == 1){
+        if (month == 12 && option == 1 && year == 2024){
+          month = 12;
+        }
+        else{
+          month++;
+        }
+        if (month > 12){
+          month = 1;
+          year++;
+        }
       }
-      if (month > 12){
-        month = 1;
-        year++;
+      else if (option == 0){
+        if (month <= 7 && option == 0 && year == 2023){
+          month = 7;
+        }
+        else{
+          month--;
+        }
+        if (month < 1){
+          month = 12;
+          year--;
+        }
       }
     }
-    else if (option == 0){
-    if(month <= 7 && option == 0 && year == 2023){
-      month = 7;
-    }
-    else{
-      month--;
-    }
-    if (month < 1){
-        month = 12;
-        year--;
-      }
-    }
-    
-    
   }
-}
   escolha_dia();
 }
 void escolha_dia(){
@@ -296,66 +296,65 @@ void escolha_dia(){
   opcao_dia = 32;
   while (opcao_dia > 31 || opcao_dia < 1){
     if (option == 2){
-        cout << "\nDigite o dia: ";
-        cin >> opcao_dia;
+      cout << "\nDigite o dia: ";
+      cin >> opcao_dia;
       switch (opcao_dia){
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-        case 16:
-        case 17:
-        case 18:
-        case 19:
-        case 20:
-        case 21:
-        case 22:
-        case 23:
-        case 24:
-        case 25:
-        case 26:
-        case 27:
-        case 28:
-          cout << "\nDia: " << opcao_dia << " foi SELECIONADO!" << endl;
-          break;
-        case 29:
-          if (month == 2 && opcao_dia == 28 || opcao_dia == 29){
-            cout << "\nDia: " << opcao_dia << " foi SELECIONADO!" << endl;
-          }
-          break;
-        case 30:
-          if (opcao_dia == 30 && month == 4 || month == 6 || month == 9 || month == 11){
-            cout << "\nDia: " << opcao_dia << " foi SELECIONADO!" << endl;
-          }
-          break;
-        case 31:
-          if (opcao_dia == 31 && month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-            cout << "\nDia: " << opcao_dia << " foi SELECIONADO!" << endl;
-          }
-          break;
-          if (opcao_dia > 31 || opcao_dia < 1){
-            cout << "\nErro, tente novamente!" << endl;
-          }
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+      case 16:
+      case 17:
+      case 18:
+      case 19:
+      case 20:
+      case 21:
+      case 22:
+      case 23:
+      case 24:
+      case 25:
+      case 26:
+      case 27:
+      case 28:
+        cout << "DATA: " << opcao_dia << "/" << month << "/" << year;
+        break;
+      case 29:
+        if (month == 2 && opcao_dia == 28 || opcao_dia == 29){
+          cout << "DATA: " << opcao_dia << "/" << month << "/" << year;
+        }
+        break;
+      case 30:
+        if (opcao_dia == 30 && month == 4 || month == 6 || month == 9 || month == 11){
+          cout << "DATA: " << opcao_dia << "/" << month << "/" << year;
+       }
+        break;
+      case 31:
+        if (opcao_dia == 31 && month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+          cout << "DATA: " << opcao_dia << "/" << month << "/" << year;
+        }
+        break;
+        if (opcao_dia > 31 || opcao_dia < 1){
+          cout << "DATA: " << opcao_dia << "/" << month << "/" << year;
         }
       }
-      if(opcao_dia == 31 || month == 2 && month == month == 4 && month == 6 && month == 9 && month == 11){
+    }
+    if (month == 2 && opcao_dia == 31 || opcao_dia == 31 && month == 4 || opcao_dia == 31 && month == 6 || opcao_dia == 31 && month == 9 || opcao_dia == 31 && month == 11){
       escolha_dia();
     }
-    if(month == 2 && opcao_dia > 29){
+    if (month == 2 && opcao_dia > 29){
       escolha_dia();
     }
-  
   }
 }
 void escolher_passagem(){
@@ -364,25 +363,27 @@ void escolher_passagem(){
   for (i = 0; i < 3; i++){
     qtd_pass[i] = 0;
   }
-  cout << "\nViajanete" << endl;
-  cout << "[0]Adulto: A partir de 12 anos!" << endl;
+  
+  cout << "\nViajante" << endl;
+  cout << "[0]Adulto: A partir de 12 anos" << endl;
   cout << "[1]Criança: 2 a 11 anos" << endl;
   cout << "[2]Bebê: de 0 a 23 meses" << endl;
 
-  cout << "\nDigite o numero:";
+  cout << "\nDigite o numero: ";
   cin >> escolherPass;
   switch (escolherPass){
   case 0:
-    cout << "[ " << qtd_pass[0] << "]";
+    cout << "[" << qtd_pass[0] << "]";
     cout << "\nQuantidade: ";
     cin >> compra_pass;
     break;
   case 1:
-    cout << "[ " << qtd_pass[1] << "]";
+    cout << "[" << qtd_pass[1] << "]";
     cout << "\nQuantidade: ";
     cin >> compra_pass;
+    break;
   case 2:
-    cout << "[ " << qtd_pass[2] << "]";
+    cout << "[" << qtd_pass[2] << "]";
     cout << "\nQuantidade: ";
     cin >> compra_pass;
     break;
@@ -393,7 +394,7 @@ void escolher_passagem(){
 }
 
 void horario(){
-// Obtendo o tempo atual em segundos desde a epoch
+  // Obtendo o tempo atual em segundos desde a epoch
   std::time_t tempoAtual = std::time(nullptr);
 
   // Convertendo o valor do tempo para uma estrutura tm
@@ -401,4 +402,15 @@ void horario(){
 
   // Mostrando o horário atual
   std::cout << "Horario: " << horaAtual->tm_hour << ":" << horaAtual->tm_min << std::endl;
+}
+void opcao_selec(){
+setlocale(LC_ALL, "Portuguese_Brazil");
+cout << endl;
+if(origem && destino >= 0){
+cout << "\nORIGEM: " << lugares[origem];
+cout << "\nDESTINO: " << lugares[destino];
+}
+if(opcao_dia <= 31 && month <= 12 || year <= 24){
+cout << "\nDATA: " << opcao_dia << "/" << month << "/" << year << endl;
+}
 }
