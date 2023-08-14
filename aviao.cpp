090cb17afb4ab9d573c91bd9639f4c05382c2d33
Vanessa <vs6615302@gmail.com>
2023-08-14 18:06:41 -0300

    Alteração na função horario_voo

--- aviao.cpp
+++ aviao.cpp
@@ -670,14 +670,77 @@ void opcao_selec(){
 void escolha_horario(){
 setlocale(LC_ALL, "Portuguese_Brazil");
 cout << "\nData de ida: " << opc_ida << "/" << month_next << "/" << year_next << endl;
+cout << lugares[origem_ida] << " -> " << lugares[dest_ida];
 horario_voo();
-if(opc_ida >= diaAtual && current_month == month_next){
-  cout << flight1.time << flight1.priceTime;
-  
-  } 
 
+
+
+cout << flight1.time << flight1.priceTime;
+  
 }
 void horario_voo(){
 setlocale(LC_ALL, "Portuguese_Brazil");
-  
+   switch (opc_ida){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    case 8:
+        break;
+    case 9:
+        break;
+    case 10:
+        break;
+    case 11:
+        break;
+    case 12:
+        break;
+    case 13:
+        break;
+    case 14:
+        break;
+    case 15:
+        break;
+    case 16:
+        break;
+    case 17:
+        break;
+    case 18:
+        break;
+    case 19:
+        break;
+    case 20:
+        break;
+    case 21:
+        break;
+    case 22:
+        break;
+    case 23:
+        break;
+    case 24:
+        break;
+    case 25:
+        break;
+    case 26:
+        break;
+    case 27:
+        break;
+    case 28:
+        break;
+    case 29:
+        break;
+    case 30:
+        break;
+    case 31:
+    }
 }
